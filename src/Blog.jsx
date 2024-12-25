a0178a598fd1e0d7683772a160c5acf434acd93c
vinayakstha <vsth005@gmail.com>
2024-12-25 21:22:15 +0545

    blog-post

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,19 @@
 function Blog() {
     return (
-        <h1>Blog</h1>
+        <>
+            <div className="blogPost">
+                <div className="blogImage">
+                    <img src="src/assets/ai.jpg" />
+                </div>
+                <div className="blogText">
+                    <h2>The AI Revolution: Transforming Technology and Beyond</h2>
+                    <p className="blogInfo">
+                        <span className="blogAuthor">vinayakstha</span>
+                    </p>
+                    <p className="blogDesc">Discover how AI is revolutionizing sectors like healthcare, education, finance, and entertainment, while also addressing global challenges such as climate change and resource management. Explore its ethical implications, potential risks, and the future it promises to create.</p>
+                </div>
+            </div>
+        </>
     )
 }
 export default Blog;
