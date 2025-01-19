e5b054cbb45e99240551ebcc414d5bf86ffc3ba7
vinayakstha <vsth005@gmail.com>
2025-01-19 20:14:00 +0545

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import './App.css';
-import Login from './components/public/Login';
-import Home from './components/public/Home';
-import Error from './components/public/Error';
-import Signup from './components/public/Signup';
-import Post from './components/public/Post';
-import PasswordReset from './components/public/PasswordReset';
-import Layout from './components/public/Layout';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Login = lazy(() => import('./components/public/Login'));
+const Home = lazy(() => import('./components/public/Home'));
+const Error = lazy(() => import('./components/public/Error'));
+const Signup = lazy(() => import('./components/public/Signup'));
+const Post = lazy(() => import('./components/public/Post'));
+const PasswordReset = lazy(() => import('./components/public/PasswordReset'));
+const Layout = lazy(() => import('./components/public/Layout'));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* <Suspense fallback={<div>Loading...</div>} /> */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/Post" element={<Post />} />
-        </Route>
-
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
-        <Route path="/PasswordReset" element={<PasswordReset />} />
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="Post" element={<Post />} />
+          </Route>
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="PasswordReset" element={<PasswordReset />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-
   );
 }
 
