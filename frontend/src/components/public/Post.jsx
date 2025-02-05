a6c10a5e8148dea65ecaa76f11c4a4bbb2266084
vinayakstha <vsth005@gmail.com>
2025-02-05 21:13:40 +0545

    post-category

--- frontend/src/components/public/Post.jsx
+++ frontend/src/components/public/Post.jsx
@@ -2,13 +2,23 @@ import Card from "./Card";
 import PostCSS from "./Post.module.css"
 function Post() {
     return (
-        <div className={PostCSS["post-container"]}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-        </div>
+        <>
+            <div className={PostCSS["select-container"]}>
+                <select className={PostCSS["select-input"]}>
+                    <option value="">Select Category</option>
+                    <option value="tech">Technology</option>
+                    <option value="health">Health</option>
+                    <option value="lifestyle">Lifestyle</option>
+                </select>
+            </div>
+            <div className={PostCSS["post-container"]}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </>
     )
 }
 export default Post;
