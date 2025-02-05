3b269999027f8a9229d8695e12b9792abf079536
vinayakstha <vsth005@gmail.com>
2025-02-05 20:59:49 +0545

    post-category

--- frontend/src/components/private/CreatePost.jsx
+++ frontend/src/components/private/CreatePost.jsx
@@ -10,9 +10,21 @@ function CreatePost() {
                 <div className={CreatePostCSS["input-field"]}>
                     <input type="text" placeholder="Description" />
                 </div>
-                <div className={CreatePostCSS["input-field"]} id={CreatePostCSS["file-input-container"]}>
-                    <input type="file" id={CreatePostCSS["file-input"]} />
+
+                <div className={CreatePostCSS["middle-fields"]}>
+                    <div className={CreatePostCSS["input-field"]} id={CreatePostCSS["file-input-container"]}>
+                        <input type="file" id={CreatePostCSS["file-input"]} />
+                    </div>
+                    <div className={CreatePostCSS["input-field"]} id={CreatePostCSS["dropdown-container"]}>
+                        <select className={CreatePostCSS["select-category"]}>
+                            <option value="">Select Category</option>
+                            <option value="tech">Technology</option>
+                            <option value="health">Health</option>
+                            <option value="lifestyle">Lifestyle</option>
+                        </select>
+                    </div>
                 </div>
+
                 <div className={CreatePostCSS["input-field"]} id={CreatePostCSS["text-area-input"]}>
                     <textarea placeholder="Content"></textarea>
                 </div>
